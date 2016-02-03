ec5c2b5551e060154eefb31d4c5f4f0552d593e0
Mark Keele <m.keele88@gmail.com>
2016-02-02 22:48:14 -0500

    sprinkle some C swag, more output

--- math/fact_iter.c
+++ math/fact_iter.c
@@ -5,47 +5,46 @@
 /* Create a program that executes a factorial function iteratively */
 
 
-int compute_factorial( int argc ) {
+int compute_factorial( int argc )
+{
+    int sum = 1;
+    int n = 1;
+    int x = 1;
 
-  int sum = 1;
-  int n = 1; 
-  int x = 1;
-
-  /*
-    printf("%s\n", "initial value of n: ");
-    printf("%d\n", n);
-    printf("%s\n", "initial value of x: ");
-    printf("%d\n", x);
-  */
-  
-   while( x <= argc ) {
-     sum = ( sum * x ); 
     /*
-      printf("%s", "sum value: ");
-      printf("%d\n", sum);
-      printf("%s", "x value: ");
+      printf("%s\n", "initial value of n: ");
+      printf("%d\n", n);
+      printf("%s\n", "initial value of x: ");
       printf("%d\n", x);
     */
-    x = (x + 1);
-  }
 
-  return sum;
+     while( x <= argc ) {
+         sum = ( sum * x );
+        /*
+          printf("%s", "sum value: ");
+          printf("%d\n", sum);
+          printf("%s", "x value: ");
+          printf("%d\n", x);
+        */
+        x = (x + 1);
+    }
 
+    return sum;
 }
 
 
 
-int main( int argc, char *argv[] ) {
+int main( int argc, char *argv[] )
+{
+    /*
+     * Take input for the coefficient of the factorial function !
+    */
 
-  /* 
-   * Take input for the coefficient of the factorial function !
-  */
+    int x = 0;
 
-  int x = 0;
+    printf("What number do you want to factorialize, you sick fuck? ");
+    fscanf(stdin, "%d", &x);
 
-  printf("What number do you want to factorialize, you sick fuck? ");
-  fscanf(stdin, "%d", &x);
-
-  printf("%s\n", "Ok, then, running factorial operation...");
-  printf( "%d\n", compute_factorial(x));
+    printf("%s\n", "Ok, then, running factorial operation...");
+    printf( "Factorial of %d is -> %d\n", x, compute_factorial(x));
 }
